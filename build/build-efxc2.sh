7ac6912645687032d0cbbc8db577f3f41bab698a
MARINA\jpmug <jpmugaas@suddenlink.net>
2024-05-12 18:19:52 -0400

    Various improvements to the build process: 1) *.err reports are copied to the build directory for review under the name of the architecture. 2) Check in a MSYS2 PKGBUILD file that I use internally to test compile efxc2 for MSYS. 3) add a bash script to call the appropriate makepkg-mingw and makepkg to build the test packages.  We can now have Win32 and Win64 distributions of this program.

---
+++ build/build-efxc2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm -rf src pkg
+MINGW_ARCH=mingw64 makepkg-mingw -f
+rm -rf *.zip
+MINGW_ARCH=mingw32 makepkg-mingw -f
+MINGW_ARCH=ucrt64 makepkg-mingw -f
+rm -rf *.zip
+MINGW_ARCH=clang64 makepkg-mingw -f
+MINGW_ARCH=clang32 makepkg-mingw -f
+cd msys
+rm -rf src pkg
+makepkg -f
