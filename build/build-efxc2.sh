7af8a3830cbd6bd9d6058cb8b571d7f76b73af28
MARINA\jpmug <jpmugaas@suddenlink.net>
2024-05-15 06:56:05 -0400

    Build improvements 1) specify log file name for PVS-Studio 2) do a little cleanup in build-efxc2.sh script. 3) add a build-clean-efxc2.sh script to cleanup after a build.

--- build/build-efxc2.sh
+++ build/build-efxc2.sh
@@ -3,6 +3,7 @@ rm -rf src pkg
 MINGW_ARCH=mingw64 makepkg-mingw -f
 rm -rf *.zip
 MINGW_ARCH=mingw32 makepkg-mingw -f
+rm -rf *.zip
 MINGW_ARCH=ucrt64 makepkg-mingw -f
 rm -rf *.zip
 MINGW_ARCH=clang64 makepkg-mingw -f
@@ -10,3 +11,7 @@ MINGW_ARCH=clang32 makepkg-mingw -f
 cd msys
 rm -rf src pkg
 makepkg -f
+cd ..
+cd ..
+rm -rf src
+rm -rf pkg
---
+++ build/clean-efxc2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm -rf *.html
+rm -rf *.zip
+rm -rf *.zst
+rm -rf src pkg
+cd msys
+rm -rf *.zst
+rm -rf src pkg
+cd ..
